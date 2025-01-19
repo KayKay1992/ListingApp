f7f52a3ef1cb7416b4a0b74c7a2a75c8c86e63a3
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-01-19 13:43:07 -0800

    All startups and Startup card.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+;
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -7,6 +8,18 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author:{_id: 1, name: 'John'},
+    title: 'The Ai Masters',
+    description: 'AI is revolutionizing various industries, but its impact on business is vast and complex.',
+    image: 'https://itchronicles.com/wp-content/uploads/2020/11/where-is-ai-used.jpg',
+    category: 'Business',
+
+    
+  }]
   return (
     <>
       <section className="pink_container">
@@ -18,6 +31,20 @@ export default async function Home({
           Submit Ideas, Vote on Pitches, and get Noticed in virtual Competitions
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+        <ul className="mt-7 card_grid">
+      {posts ?. length > 0 ? (
+        posts.map((post: StartupCardType) =>(
+          <StartupCard key={post?._id} post={post}/>
+        ))
+      ): (
+        <p className="no-results">No Startups found</p>
+      )}
+        </ul>
       </section>
     </>
   );
