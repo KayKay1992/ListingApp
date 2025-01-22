f7fc934205dd9f1e04727163a97846a6402a0b01
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-01-22 14:01:18 -0800

    CREATING SANITY SCHEMA

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from 'next/font/local'
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// import 'easymde/dist/easymde.min.css';
+import "easymde/dist/easymde.min.css";
 
 const workSans = localFont({
   src: [
